658c4d215eaae70df492e16c3fafc691576e9229
Chaluka Rathnayake <chaluka.rathnayaka@gmail.com>
2026-02-11 00:20:26 +0100

    inital commit

---
+++ eshopping/eshopping.client/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using eshopping.client.Data;
+using eshopping.client.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eshopping.client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(ProductContext.Products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ eshopping/eshopping.client/Data/ProductContext.cs
@@ -0,0 +1,44 @@
+﻿using eshopping.client.Models;
+
+namespace eshopping.client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                Name = "Laptop",
+                Category = "Laptop",
+                Description = "A high-performance laptop suitable for all your computing needs.",
+                ImageFile = "laptop.jpg",
+                Price = 999.99m
+            },
+            new Product
+            {
+                Name = "Smartphone",
+                Category = "Smart Phone",
+                Description = "A latest-generation smartphone with cutting-edge features.",
+                ImageFile = "smartphone.jpg",
+                Price = 699.99m
+            },
+            new Product
+            {
+                Name = "Headphones",
+                Category = "Accessories",
+                Description = "Noise-cancelling headphones for an immersive audio experience.",
+                ImageFile = "headphones.jpg",
+                Price = 199.99m
+
+            },
+            new Product
+            {
+                Name = "Coffee Maker",
+                Category = "Home Appliances",
+                Description = "Brew the perfect cup of coffee with this easy-to-use coffee maker.",
+                ImageFile = "coffeemaker.jpg",
+                Price = 49.99m
+            }
+        };
+    }
+}
